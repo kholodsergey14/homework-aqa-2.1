ddea30caf81c25f63b21da59df81453302b972cc
Kholod Sergey <kholodsergey@yandex.ru>
2020-03-23 13:26:42 +0500

    Update test4

--- src/test/java/ru/netology/request/RequestTest.java
+++ src/test/java/ru/netology/request/RequestTest.java
@@ -1,6 +1,7 @@
 package ru.netology.request;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RequestTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\UserSK\\Desktop\\chromedriver.exe");
+    }
 
     @BeforeEach
     void setup() {
